4a1dce8fb13bd89e10dba73f7e7dae5241c83dce
Rhuan Rocha <rhuan080@gmail.com>
2021-02-13 12:43:52 -0300

    Adding project

---
+++ src/main/java/net/rhuanrocha/speaker/Speaker.java
@@ -0,0 +1,14 @@
+package net.rhuanrocha.speaker;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Speaker extends PanacheEntity {
+
+    public String name;
+
+    public String minibio;
+
+}
---
+++ src/main/java/net/rhuanrocha/speaker/SpeakerResource.java
@@ -0,0 +1,6 @@
+package net.rhuanrocha.speaker;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface SpeakerResource extends PanacheEntityResource<Speaker,Long> {
+}
