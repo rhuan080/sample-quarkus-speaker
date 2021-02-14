c886498076a83edc2b2b7b2f3411ad3fb7125daa
Rhuan Rocha <rhuan080@gmail.com>
2021-02-13 22:10:44 -0300

    Creating a SpeakerResource

--- src/main/java/net/rhuanrocha/speaker/SpeakerResource.java
+++ src/main/java/net/rhuanrocha/speaker/SpeakerResource.java
@@ -1,6 +1,36 @@
 package net.rhuanrocha.speaker;
 
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 
-public interface SpeakerResource extends PanacheEntityResource<Speaker,Long> {
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
+
+@Path("/speakers")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
+public class SpeakerResource {
+
+    @GET
+    public List<Speaker> list(){
+        return Speaker.listAll();
+    }
+
+    @POST
+    public Response save (Speaker speaker){
+        speaker.id = null;
+        speaker.persist();
+        return Response.created(URI.create(String.format("/speakers/{id}", speaker.id))).build();
+    }
+
+    @GET
+    @Path("/event/{idEvent}")
+    public List<Speaker> listByEvent(@PathParam("idEvent") Long idEvent){
+        return Speaker.list("idEvent", idEvent);
+    }
+
+
 }
